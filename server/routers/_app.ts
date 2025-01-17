ad4a49cf5ab9079f9e6a10553f3608774b3f2e61
matteyu <matt.r.ilagan@gmail.com>
2025-01-17 09:05:14 -0800

    cleanup imports

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,5 +1,6 @@
 
-import { z } from 'zod';import { publicProcedure, protectedProcedure, router } from "../trpc"
+import { z } from 'zod';
+import { publicProcedure, protectedProcedure, router } from "../trpc"
 import { signJWT } from "../auth"
 // import { supabase } from '@/utils/supabaseClient';
 
