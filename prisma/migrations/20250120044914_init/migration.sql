3b9a518b6241aa692661259c092af544882b6e84
matteyu <matt.r.ilagan@gmail.com>
2025-01-19 20:50:14 -0800

    fix: add varchar, indexes, and application references

--- prisma/migrations/20250120044914_init/migration.sql
+++ prisma/migrations/20250120044914_init/migration.sql
@@ -31,9 +31,9 @@ CREATE TYPE "ChallengePurpose" AS ENUM ('ACTIVATION', 'SHARE_RECOVERY', 'SHARE_R
 -- CreateTable
 CREATE TABLE "AuthMethods" (
     "id" TEXT NOT NULL,
-    "providerId" TEXT NOT NULL,
+    "providerId" VARCHAR(255) NOT NULL,
     "providerType" "AuthProviderType" NOT NULL,
-    "providerLabel" TEXT NOT NULL,
+    "providerLabel" VARCHAR(255) NOT NULL,
     "linkedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "lastUsedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "unlinkedAt" TIMESTAMP(3),
@@ -45,13 +45,13 @@ CREATE TABLE "AuthMethods" (
 -- CreateTable
 CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
-    "name" TEXT,
-    "email" TEXT,
+    "name" VARCHAR(100),
+    "email" VARCHAR(255),
     "notificationsSetting" "NotificationSetting" NOT NULL DEFAULT 'SECURITY',
     "recoveryWalletsRequiredSetting" INTEGER NOT NULL DEFAULT 1,
-    "ipFilterSetting" TEXT,
+    "ipFilterSetting" VARCHAR(255),
     "ipPrivacyFilterSetting" "FilterPrivacySetting" DEFAULT 'HIDE_DETAILS',
-    "countryFilterSetting" TEXT,
+    "countryFilterSetting" VARCHAR(2),
     "countryPrivacySetting" "FilterPrivacySetting" DEFAULT 'HIDE_DETAILS',
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
@@ -60,9 +60,9 @@ CREATE TABLE "Users" (
 -- CreateTable
 CREATE TABLE "Developers" (
     "id" SERIAL NOT NULL,
-    "plan" TEXT NOT NULL DEFAULT 'free',
-    "planPaidAt" TEXT NOT NULL DEFAULT 'free',
-    "apiKey" TEXT NOT NULL,
+    "plan" VARCHAR(50) NOT NULL DEFAULT 'free',
+    "planPaidAt" VARCHAR(50) NOT NULL DEFAULT 'free',
+    "apiKey" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
@@ -73,11 +73,11 @@ CREATE TABLE "Developers" (
 -- CreateTable
 CREATE TABLE "Applications" (
     "id" TEXT NOT NULL,
-    "domains" TEXT[],
+    "domains" VARCHAR(255)[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "settings" JSONB NOT NULL,
-    "auth0ClientId" TEXT NOT NULL,
+    "auth0ClientId" VARCHAR(255) NOT NULL,
     "ownerId" INTEGER NOT NULL,
 
     CONSTRAINT "Applications_pkey" PRIMARY KEY ("id")
@@ -90,11 +90,11 @@ CREATE TABLE "Wallets" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "chain" "Chain" NOT NULL,
-    "address" TEXT NOT NULL,
-    "publicKey" TEXT,
+    "address" VARCHAR(255) NOT NULL,
+    "publicKey" VARCHAR(255),
     "identifierTypeSetting" "WalletIdentifierType" NOT NULL DEFAULT 'ALIAS',
-    "descriptionSetting" TEXT,
-    "tagsSetting" TEXT[],
+    "descriptionSetting" VARCHAR(500),
+    "tagsSetting" VARCHAR(50)[],
     "doNotAskAgainSetting" BOOLEAN NOT NULL DEFAULT true,
     "walletPrivacySetting" "WalletPrivacySetting" NOT NULL DEFAULT 'PUBLIC',
     "canRecoverAccountSetting" BOOLEAN NOT NULL DEFAULT true,
@@ -104,7 +104,7 @@ CREATE TABLE "Wallets" (
     "recoveryAuthsRequiredSetting" INTEGER NOT NULL DEFAULT 1,
     "info" JSONB NOT NULL,
     "source" JSONB NOT NULL,
-    "location" TEXT NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
     "userId" TEXT NOT NULL,
 
     CONSTRAINT "Wallets_pkey" PRIMARY KEY ("id")
@@ -114,7 +114,7 @@ CREATE TABLE "Wallets" (
 CREATE TABLE "WalletActivations" (
     "id" TEXT NOT NULL,
     "activatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "location" TEXT NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
     "walletId" TEXT NOT NULL,
     "workKeyShareId" TEXT NOT NULL,
 
@@ -125,7 +125,7 @@ CREATE TABLE "WalletActivations" (
 CREATE TABLE "WalletRecoveries" (
     "id" TEXT NOT NULL,
     "recoveredAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "location" TEXT NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
     "walletId" TEXT NOT NULL,
     "recoveryKeyShareId" TEXT NOT NULL,
 
@@ -137,7 +137,7 @@ CREATE TABLE "WalletExports" (
     "id" TEXT NOT NULL,
     "type" "ExportType" NOT NULL,
     "exportedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "location" TEXT NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
     "walletId" TEXT NOT NULL,
 
     CONSTRAINT "WalletExports_pkey" PRIMARY KEY ("id")
@@ -146,15 +146,15 @@ CREATE TABLE "WalletExports" (
 -- CreateTable
 CREATE TABLE "WorkKeyShare" (
     "id" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
+    "status" VARCHAR(50) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "sharesRotatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "rotationWarnings" INTEGER NOT NULL DEFAULT 0,
-    "deviceNonce" TEXT NOT NULL,
-    "location" TEXT NOT NULL,
-    "authShare" TEXT NOT NULL,
-    "deviceShareHash" TEXT NOT NULL,
-    "deviceSharePublicKey" TEXT NOT NULL,
+    "deviceNonce" VARCHAR(255) NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
+    "authShare" VARCHAR(255) NOT NULL,
+    "deviceShareHash" VARCHAR(255) NOT NULL,
+    "deviceSharePublicKey" VARCHAR(255) NOT NULL,
     "walletId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
 
@@ -164,12 +164,12 @@ CREATE TABLE "WorkKeyShare" (
 -- CreateTable
 CREATE TABLE "RecoveryKeyShare" (
     "id" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
+    "status" VARCHAR(50) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "location" TEXT NOT NULL,
-    "recoveryAuthShare" TEXT NOT NULL,
-    "recoveryBackupShareHash" TEXT NOT NULL,
-    "recoveryBackupSharePublicKey" TEXT NOT NULL,
+    "location" VARCHAR(255) NOT NULL,
+    "recoveryAuthShare" VARCHAR(255) NOT NULL,
+    "recoveryBackupShareHash" VARCHAR(255) NOT NULL,
+    "recoveryBackupSharePublicKey" VARCHAR(255) NOT NULL,
     "walletId" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
 
@@ -181,8 +181,8 @@ CREATE TABLE "Challenges" (
     "id" TEXT NOT NULL,
     "type" "ChallengeType" NOT NULL,
     "purpose" "ChallengePurpose" NOT NULL,
-    "value" TEXT NOT NULL,
-    "version" TEXT NOT NULL,
+    "value" VARCHAR(255) NOT NULL,
+    "version" VARCHAR(50) NOT NULL,
     "issuedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT,
     "walletId" TEXT,
@@ -194,10 +194,10 @@ CREATE TABLE "Challenges" (
 CREATE TABLE "DevicesAndLocations" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "deviceNonce" TEXT NOT NULL,
-    "ip" TEXT NOT NULL,
+    "deviceNonce" VARCHAR(255) NOT NULL,
+    "ip" VARCHAR(45) NOT NULL,
     "userAgent" TEXT NOT NULL,
-    "domain" TEXT NOT NULL,
+    "applicationid" TEXT,
     "userId" TEXT,
 
     CONSTRAINT "DevicesAndLocations_pkey" PRIMARY KEY ("id")
@@ -206,13 +206,12 @@ CREATE TABLE "DevicesAndLocations" (
 -- CreateTable
 CREATE TABLE "Sessions" (
     "id" TEXT NOT NULL,
-    "providerSessionId" TEXT NOT NULL,
+    "providerSessionId" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "deviceNonce" TEXT NOT NULL,
-    "ip" TEXT NOT NULL,
-    "userAgent" TEXT NOT NULL,
-    "domains" TEXT[],
+    "deviceNonce" VARCHAR(255) NOT NULL,
+    "ip" VARCHAR(45) NOT NULL,
+    "userAgent" VARCHAR(500) NOT NULL,
     "userId" TEXT,
 
     CONSTRAINT "Sessions_pkey" PRIMARY KEY ("id")
@@ -222,15 +221,32 @@ CREATE TABLE "Sessions" (
 CREATE TABLE "EventLog" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
-    "eventType" TEXT NOT NULL,
+    "eventType" VARCHAR(100) NOT NULL,
     "details" JSONB NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "EventLog_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "_ApplicationToSession" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_ApplicationToSession_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "AuthMethods_providerId_idx" ON "AuthMethods"("providerId");
+
+-- CreateIndex
+CREATE INDEX "AuthMethods_lastUsedAt_idx" ON "AuthMethods"("lastUsedAt");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "AuthMethods_userId_providerId_key" ON "AuthMethods"("userId", "providerId");
+
+-- CreateIndex
+CREATE INDEX "Users_email_idx" ON "Users"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Developers_apiKey_key" ON "Developers"("apiKey");
@@ -239,67 +255,154 @@ CREATE UNIQUE INDEX "Developers_apiKey_key" ON "Developers"("apiKey");
 CREATE UNIQUE INDEX "Developers_userId_key" ON "Developers"("userId");
 
 -- CreateIndex
+CREATE INDEX "Developers_apiKey_idx" ON "Developers"("apiKey");
+
+-- CreateIndex
+CREATE INDEX "Applications_auth0ClientId_idx" ON "Applications"("auth0ClientId");
+
+-- CreateIndex
+CREATE INDEX "Applications_ownerId_idx" ON "Applications"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "Wallets_address_idx" ON "Wallets"("address");
+
+-- CreateIndex
+CREATE INDEX "Wallets_chain_idx" ON "Wallets"("chain");
+
+-- CreateIndex
+CREATE INDEX "Wallets_status_idx" ON "Wallets"("status");
+
+-- CreateIndex
+CREATE INDEX "Wallets_createdAt_idx" ON "Wallets"("createdAt");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Wallets_userId_address_chain_key" ON "Wallets"("userId", "address", "chain");
+
+-- CreateIndex
+CREATE INDEX "WalletActivations_activatedAt_idx" ON "WalletActivations"("activatedAt");
+
+-- CreateIndex
+CREATE INDEX "WalletRecoveries_recoveredAt_idx" ON "WalletRecoveries"("recoveredAt");
+
+-- CreateIndex
+CREATE INDEX "WalletExports_exportedAt_idx" ON "WalletExports"("exportedAt");
+
+-- CreateIndex
+CREATE INDEX "WorkKeyShare_status_idx" ON "WorkKeyShare"("status");
+
+-- CreateIndex
+CREATE INDEX "WorkKeyShare_sharesRotatedAt_idx" ON "WorkKeyShare"("sharesRotatedAt");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "WorkKeyShare_userId_deviceNonce_walletId_key" ON "WorkKeyShare"("userId", "deviceNonce", "walletId");
 
 -- CreateIndex
+CREATE INDEX "RecoveryKeyShare_status_idx" ON "RecoveryKeyShare"("status");
+
+-- CreateIndex
+CREATE INDEX "RecoveryKeyShare_createdAt_idx" ON "RecoveryKeyShare"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Challenges_issuedAt_idx" ON "Challenges"("issuedAt");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Challenges_type_userId_key" ON "Challenges"("type", "userId");
+
+-- CreateIndex
+CREATE INDEX "DevicesAndLocations_deviceNonce_idx" ON "DevicesAndLocations"("deviceNonce");
+
+-- CreateIndex
+CREATE INDEX "DevicesAndLocations_ip_idx" ON "DevicesAndLocations"("ip");
+
+-- CreateIndex
+CREATE INDEX "DevicesAndLocations_createdAt_idx" ON "DevicesAndLocations"("createdAt");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "DevicesAndLocations_userId_deviceNonce_ip_userAgent_key" ON "DevicesAndLocations"("userId", "deviceNonce", "ip", "userAgent");
 
 -- CreateIndex
+CREATE INDEX "Sessions_providerSessionId_idx" ON "Sessions"("providerSessionId");
+
+-- CreateIndex
+CREATE INDEX "Sessions_deviceNonce_idx" ON "Sessions"("deviceNonce");
+
+-- CreateIndex
+CREATE INDEX "Sessions_ip_idx" ON "Sessions"("ip");
+
+-- CreateIndex
+CREATE INDEX "Sessions_updatedAt_idx" ON "Sessions"("updatedAt");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Sessions_userId_deviceNonce_key" ON "Sessions"("userId", "deviceNonce");
 
--- AddForeignKey
-ALTER TABLE "AuthMethods" ADD CONSTRAINT "AuthMethods_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateIndex
+CREATE INDEX "EventLog_userId_idx" ON "EventLog"("userId");
+
+-- CreateIndex
+CREATE INDEX "EventLog_eventType_idx" ON "EventLog"("eventType");
+
+-- CreateIndex
+CREATE INDEX "EventLog_createdAt_idx" ON "EventLog"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "_ApplicationToSession_B_index" ON "_ApplicationToSession"("B");
 
 -- AddForeignKey
-ALTER TABLE "Developers" ADD CONSTRAINT "Developers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "AuthMethods" ADD CONSTRAINT "AuthMethods_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Applications" ADD CONSTRAINT "Applications_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Developers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Developers" ADD CONSTRAINT "Developers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Wallets" ADD CONSTRAINT "Wallets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Applications" ADD CONSTRAINT "Applications_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Developers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WalletActivations" ADD CONSTRAINT "WalletActivations_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Wallets" ADD CONSTRAINT "Wallets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WalletActivations" ADD CONSTRAINT "WalletActivations_workKeyShareId_fkey" FOREIGN KEY ("workKeyShareId") REFERENCES "WorkKeyShare"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WalletActivations" ADD CONSTRAINT "WalletActivations_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WalletRecoveries" ADD CONSTRAINT "WalletRecoveries_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WalletActivations" ADD CONSTRAINT "WalletActivations_workKeyShareId_fkey" FOREIGN KEY ("workKeyShareId") REFERENCES "WorkKeyShare"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WalletRecoveries" ADD CONSTRAINT "WalletRecoveries_recoveryKeyShareId_fkey" FOREIGN KEY ("recoveryKeyShareId") REFERENCES "RecoveryKeyShare"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WalletRecoveries" ADD CONSTRAINT "WalletRecoveries_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WalletExports" ADD CONSTRAINT "WalletExports_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WalletRecoveries" ADD CONSTRAINT "WalletRecoveries_recoveryKeyShareId_fkey" FOREIGN KEY ("recoveryKeyShareId") REFERENCES "RecoveryKeyShare"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WorkKeyShare" ADD CONSTRAINT "WorkKeyShare_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WalletExports" ADD CONSTRAINT "WalletExports_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WorkKeyShare" ADD CONSTRAINT "WorkKeyShare_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WorkKeyShare" ADD CONSTRAINT "WorkKeyShare_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "RecoveryKeyShare" ADD CONSTRAINT "RecoveryKeyShare_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "WorkKeyShare" ADD CONSTRAINT "WorkKeyShare_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "RecoveryKeyShare" ADD CONSTRAINT "RecoveryKeyShare_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "RecoveryKeyShare" ADD CONSTRAINT "RecoveryKeyShare_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Challenges" ADD CONSTRAINT "Challenges_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "RecoveryKeyShare" ADD CONSTRAINT "RecoveryKeyShare_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Challenges" ADD CONSTRAINT "Challenges_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Challenges" ADD CONSTRAINT "Challenges_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Challenges" ADD CONSTRAINT "Challenges_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DevicesAndLocations" ADD CONSTRAINT "DevicesAndLocations_applicationid_fkey" FOREIGN KEY ("applicationid") REFERENCES "Applications"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "DevicesAndLocations" ADD CONSTRAINT "DevicesAndLocations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Sessions" ADD CONSTRAINT "Sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ApplicationToSession" ADD CONSTRAINT "_ApplicationToSession_A_fkey" FOREIGN KEY ("A") REFERENCES "Applications"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ApplicationToSession" ADD CONSTRAINT "_ApplicationToSession_B_fkey" FOREIGN KEY ("B") REFERENCES "Sessions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
