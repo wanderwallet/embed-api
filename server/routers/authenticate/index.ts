edc5e1ecb19ec6459b59053aec7eff74dd138b6e
matteyu <matt.r.ilagan@gmail.com>
2025-01-24 08:59:48 -0800

    add auth methods for passkeys

--- server/routers/authenticate/index.ts
+++ server/routers/authenticate/index.ts
@@ -1,46 +1,63 @@
-import { publicProcedure, protectedProcedure } from "../../trpc"
-import {getUser} from '../../../lib/supabaseClient'
-import { loginWithGoogle,logoutUser, refreshSession } from "../../../services/auth"
-import { z } from "zod"
-import { googleRoutes } from "./authProviders/google"
+import { publicProcedure, protectedProcedure } from "../../trpc";
+import { getUser } from "../../../lib/supabaseClient";
+import {
+  loginWithGoogle,
+  logoutUser,
+  refreshSession,
+  startAuthenticateWithPasskeys,
+  verifyAuthenticateWithPasskeys,
+} from "../../../services/auth";
+import { z } from "zod";
+import { googleRoutes } from "./authProviders/google";
 
 enum AuthProviderType {
-    PASSKEYS = "PASSKEYS",
-    EMAIL_N_PASSWORD = "EMAIL_N_PASSWORD",
-    GOOGLE = "GOOGLE",
-    FACEBOOK = "FACEBOOK",
-    X = "X",
-    APPLE = "APPLE",
-  }
+  PASSKEYS = "PASSKEYS",
+  EMAIL_N_PASSWORD = "EMAIL_N_PASSWORD",
+  GOOGLE = "GOOGLE",
+  FACEBOOK = "FACEBOOK",
+  X = "X",
+  APPLE = "APPLE",
+}
 
 export const authenticateRouter = {
   ...googleRoutes,
   authenticate: publicProcedure
-  .input(z.object({ authProviderType: z.string(), options: z.any().optional() }))
-  .mutation(async ({input}) => {
-    if(AuthProviderType.GOOGLE === input.authProviderType){
-        const url = await loginWithGoogle(input.authProviderType)
-        return { url: url }
-    }
-    if(AuthProviderType.PASSKEYS === input.authProviderType){
-      const url = await loginWithGoogle(input.authProviderType)
-      return { url: url }
-  }
-    return
-  }),
+    .input(
+      z.object({ authProviderType: z.string(), options: z.any().optional() })
+    )
+    .mutation(async ({ input }) => {
+      if (AuthProviderType.GOOGLE === input.authProviderType) {
+        const url = await loginWithGoogle(input.authProviderType);
+        return { url: url };
+      }
+      if (AuthProviderType.PASSKEYS === input.authProviderType) {
+        if (input?.options?.type === "authenticate")
+          return await startAuthenticateWithPasskeys(
+            input.authProviderType,
+            input.options?.userId
+          );
+        if (input?.options?.type === "verify")
+            return await verifyAuthenticateWithPasskeys(
+              input.authProviderType,
+              input.options?.userId,
+              input.options?.assertionResponse
+            );
+      }
+      return;
+    }),
 
   getUser: protectedProcedure.query(async () => {
-    const user = await getUser()
-    return { user }
+    const user = await getUser();
+    return { user };
   }),
 
   logout: protectedProcedure.mutation(async () => {
-    await logoutUser()
-    return { success: true, message: "Logged out successfully" }
+    await logoutUser();
+    return { success: true, message: "Logged out successfully" };
   }),
 
   refreshSession: protectedProcedure.query(async () => {
-    const { session, user } = await refreshSession()
+    const { session, user } = await refreshSession();
     return {
       message: "Session refreshed successfully.",
       user,
@@ -48,7 +65,6 @@ export const authenticateRouter = {
         expires_at: session?.expires_at,
         expires_in: session?.expires_in,
       },
-    }
+    };
   }),
-}
-
+};
