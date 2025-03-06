11a5ff8f14c5f74ecea0c60f76806e331f459200
matteyu <matt.r.ilagan@gmail.com>
2025-03-05 18:18:33 -0800

    feat: add social auth and email and password

--- server/routers/authenticate.ts
+++ server/routers/authenticate.ts
@@ -1,6 +1,12 @@
-import { publicProcedure, protectedProcedure } from "../trpc"
-import { loginWithGoogle, logoutUser, refreshSession, getUser } from "../services/auth"
-import { z } from "zod"
+import { publicProcedure, protectedProcedure } from "../trpc";
+import {
+  loginWithGoogle,
+  logoutUser,
+  refreshSession,
+  getUser,
+} from "../services/auth";
+import { z } from "zod";
+import { createServerClient } from "../utils/supabase/supabase-server-client";
 
 enum AuthProviderType {
   PASSKEYS = "PASSKEYS",
@@ -16,37 +22,76 @@ export const authenticateRouter = {
     return {
       user: ctx.user,
       session: ctx.session,
-    }
+    };
   }),
 
   authenticate: publicProcedure
-  .input(z.object({ authProviderType: z.string() }))
-  .mutation(async ({input}) => {
-    let url = ''
+    .input(
+      z.object({
+        authProviderType: z.string(),
+        options: z
+          .object({ email: z.string(), password: z.string() })
+          .optional(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      let url = "";
 
-    if (AuthProviderType.GOOGLE === input.authProviderType){
-        url = await loginWithGoogle(input.authProviderType)
+      const supabase = await createServerClient();
+      let response;
+      switch (input.authProviderType) {
+        case AuthProviderType.GOOGLE:
+          const url = await loginWithGoogle(input.authProviderType);
+          return { url };
 
-        console.log("URL =", url);
+        case AuthProviderType.FACEBOOK:
+          response = await supabase.auth.signInWithOAuth({
+            provider: "facebook",
+          });
+          return { url: response.data.url };
 
-        return { url: url }
-    }
+        case AuthProviderType.X:
+          response = await supabase.auth.signInWithOAuth({
+            provider: "twitter",
+          });
+          return { url: response.data.url };
 
-    return { url: url }
-  }),
+        case AuthProviderType.APPLE:
+          response = await supabase.auth.signInWithOAuth({ provider: "apple" });
+          return { url: response.data.url };
+
+        case AuthProviderType.EMAIL_N_PASSWORD:
+          if (!input?.options?.email || !input?.options?.password) {
+            throw new Error(
+              "Email and password are required for this login method"
+            );
+          }
+          const { error, data } = await supabase.auth.signInWithPassword({
+            email: input.options.email,
+            password: input.options.password,
+          });
+          if (error) {
+            throw new Error(error.message);
+          }
+          return { user: data.user };
+
+        default:
+          throw new Error("Unsupported auth provider type");
+      }
+    }),
 
   getUser: protectedProcedure.query(async () => {
-    const user = await getUser()
-    return { user }
+    const user = await getUser();
+    return { user };
   }),
 
   logout: protectedProcedure.mutation(async () => {
-    await logoutUser()
-    return { success: true, message: "Logged out successfully" }
+    await logoutUser();
+    return { success: true, message: "Logged out successfully" };
   }),
 
   refreshSession: protectedProcedure.mutation(async () => {
-    const { session, user } = await refreshSession()
+    const { session, user } = await refreshSession();
     return {
       message: "Session refreshed successfully.",
       user,
@@ -54,7 +99,6 @@ export const authenticateRouter = {
         expires_at: session?.expires_at,
         expires_in: session?.expires_in,
       },
-    }
+    };
   }),
-}
-
+};
