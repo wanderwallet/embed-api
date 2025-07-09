7f16bd277d9c7d0b59053b3a5bc03a008d90c09c
Dani Gámez Franco <gmzcodes@outlook.com>
2025-07-09 11:36:20 +0200

    Update SupabaseUserMetadata and SDK build.

--- server/utils/supabase/supabase.types.ts
+++ server/utils/supabase/supabase.types.ts
@@ -2,7 +2,16 @@
 import type { User } from "@supabase/supabase-js";
 
 export interface SupabaseUserMetadata {
-  hasPassword: boolean;
+  hasPassword?: boolean;
+  email_verified?: boolean;
+  phone_verified?: boolean;
+  email?: string;
+  user_name?: string;
+  preferred_username?: string;
+  name?: string;
+  full_name?: string;
+  avatar_url?: string;
+  picture?: string;
 }
 
 export interface SupabaseUser extends User {
