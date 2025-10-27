9cba20e63e9bc9c8fe9e626cbc027bb32cc6794d
Pawan Paudel <pawanpaudel93@gmail.com>
2025-10-27 14:06:38 +0545

    refactor: remove prisma wait & timeout

--- server/routers/wallets/createPublicWallet.ts
+++ server/routers/wallets/createPublicWallet.ts
@@ -62,71 +62,65 @@ export const createPublicWallet = protectedProcedure
     const canBeRecovered =
       input.source.type === WalletSourceType.IMPORTED ? true : false;
 
-    const wallet = await ctx.prisma.$transaction(
-      async (tx) => {
-        const deleteWalletsPromise = tx.wallet.deleteMany({
-          where: {
-            userId: ctx.user.id,
-          },
-        });
+    const wallet = await ctx.prisma.$transaction(async (tx) => {
+      const deleteWalletsPromise = tx.wallet.deleteMany({
+        where: {
+          userId: ctx.user.id,
+        },
+      });
 
-        const deleteWorkKeySharesPromise = tx.workKeyShare.deleteMany({
-          where: {
-            userId: ctx.user.id,
-          },
-        });
+      const deleteWorkKeySharesPromise = tx.workKeyShare.deleteMany({
+        where: {
+          userId: ctx.user.id,
+        },
+      });
 
-        const deleteRecoveryKeySharesPromise = tx.recoveryKeyShare.deleteMany({
-          where: {
-            userId: ctx.user.id,
-          },
-        });
+      const deleteRecoveryKeySharesPromise = tx.recoveryKeyShare.deleteMany({
+        where: {
+          userId: ctx.user.id,
+        },
+      });
 
-        // Temporarily, because users can only have one wallet (for now), we
-        // simply delete old ones if a new one is created:
+      // Temporarily, because users can only have one wallet (for now), we
+      // simply delete old ones if a new one is created:
 
-        await Promise.all([
-          deleteWalletsPromise,
-          deleteWorkKeySharesPromise,
-          deleteRecoveryKeySharesPromise,
-        ]);
+      await Promise.all([
+        deleteWalletsPromise,
+        deleteWorkKeySharesPromise,
+        deleteRecoveryKeySharesPromise,
+      ]);
 
-        return tx.wallet.create({
-          data: {
-            status: input.status,
-            chain: input.chain,
-            address: input.address,
-            publicKey: input.publicKey,
-            aliasSetting: input.aliasSetting,
-            descriptionSetting: input.descriptionSetting,
-            tagsSetting: input.tagsSetting,
-            walletPrivacySetting: WalletPrivacySetting.PUBLIC,
-            canRecoverAccountSetting: input.canRecoverAccountSetting,
-            canBeRecovered,
-            source: input.source as InputJsonValue,
+      return tx.wallet.create({
+        data: {
+          status: input.status,
+          chain: input.chain,
+          address: input.address,
+          publicKey: input.publicKey,
+          aliasSetting: input.aliasSetting,
+          descriptionSetting: input.descriptionSetting,
+          tagsSetting: input.tagsSetting,
+          walletPrivacySetting: WalletPrivacySetting.PUBLIC,
+          canRecoverAccountSetting: input.canRecoverAccountSetting,
+          canBeRecovered,
+          source: input.source as InputJsonValue,
 
-            userProfile: getUserConnectOrCreate(ctx),
+          userProfile: getUserConnectOrCreate(ctx),
 
-            deviceAndLocation: getDeviceAndLocationConnectOrCreate(ctx),
+          deviceAndLocation: getDeviceAndLocationConnectOrCreate(ctx),
 
-            workKeyShares: {
-              create: {
-                authShare: input.authShare,
-                deviceShareHash: input.deviceShareHash,
-                deviceSharePublicKey: input.deviceSharePublicKey,
-                userId: ctx.user.id,
-                sessionId: ctx.session.id,
-                deviceNonce: ctx.session.deviceNonce,
-              },
+          workKeyShares: {
+            create: {
+              authShare: input.authShare,
+              deviceShareHash: input.deviceShareHash,
+              deviceSharePublicKey: input.deviceSharePublicKey,
+              userId: ctx.user.id,
+              sessionId: ctx.session.id,
+              deviceNonce: ctx.session.deviceNonce,
             },
           },
-        });
-      },
-      {
-        maxWait: 20000,
-        timeout: 20000,
-      }
-    );
+        },
+      });
+    });
 
     return {
       wallet: wallet as DbWallet,
